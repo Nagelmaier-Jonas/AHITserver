ebe2e44f6318b4b2f9b6ae45a286f42bb47f3145
JonasNagi <j.nagelmaier@htlkrems.at>
2021-01-27 19:59:04 +0100

    Save

---
+++ src/at/ahit/server/playerValues/PlayerStats.java
@@ -0,0 +1,12 @@
+package at.ahit.server.playerValues;
+
+public class PlayerStats {
+    private int Amount;
+    private boolean Overlay;
+    private int CookLevel;
+    private int FarmerLevel;
+    private int HunterLevel;
+    private int LumberjackLevel;
+    private int MinerLevel;
+    private int MonsterHunterLevel;
+}
