28c42325ab6dc57efcb02b73b209ae3e0ce69552
JonasNagi <j.nagelmaier@htlkrems.at>
2021-01-27 15:58:39 +0100

    onEnable

--- src/at/ahit/server/main/Main.java
+++ src/at/ahit/server/main/Main.java
@@ -3,6 +3,10 @@ package at.ahit.server.main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    System.out.println("Hallo");
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hallo");
+    }
 
 }
