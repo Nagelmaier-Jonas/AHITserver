3c83d93eb66216a4831d956e394f5375b311ab78
JonasNagi <j.nagelmaier@htlkrems.at>
2021-01-27 20:20:43 +0100

    stat

---
+++ src/at/ahit/server/enums/JobLevels.java
@@ -0,0 +1,20 @@
+package at.ahit.server.enums;
+
+public enum JobLevels {
+    JOB_LEVELS_ONE(100),
+    JOB_LEVELS_TWO(100),
+    JOB_LEVELS_THREE(100),
+    JOB_LEVELS_FOUR(100),
+    JOB_LEVELS_FIVE(100),
+    JOB_LEVELS_SIX(100),
+    JOB_LEVELS_SEVEN(100),
+    JOB_LEVELS_EIGHT(100),
+    JOB_LEVELS_NINE(100),
+    JOB_LEVELS_TEN(100);
+
+    private final int level;
+
+    JobLevels(int level) {
+        this.level = level;
+    }
+}
--- src/at/ahit/server/playerValues/PlayerStats.java
+++ src/at/ahit/server/playerValues/PlayerStats.java
@@ -3,10 +3,73 @@ package at.ahit.server.playerValues;
 public class PlayerStats {
     private int Amount;
     private boolean Overlay;
+
     private int CookLevel;
     private int FarmerLevel;
     private int HunterLevel;
     private int LumberjackLevel;
     private int MinerLevel;
     private int MonsterHunterLevel;
+
+    private int CookExp;
+    private int FarmerExp;
+    private int HunterExp;
+    private int LumberjackExp;
+    private int MinerExp;
+    private int MonsterHunterExp;
+
+    private boolean[] CookSkills = new boolean[1];
+    private boolean[] FarmerSkills = new boolean[1];
+    private boolean[] HunterSkills = new boolean[1];
+    private boolean[] LumberjackSkills = new boolean[1];
+    private boolean[] MinerSkills = new boolean[1];
+    private boolean[] MonsterHunterSkills = new boolean[1];
+
+    public boolean[] getCookSkills() {
+        return CookSkills;
+    }
+
+    public void setCookSkills(boolean[] cookSkills) {
+        CookSkills = cookSkills;
+    }
+
+    public boolean[] getFarmerSkills() {
+        return FarmerSkills;
+    }
+
+    public void setFarmerSkills(boolean[] farmerSkills) {
+        FarmerSkills = farmerSkills;
+    }
+
+    public boolean[] getHunterSkills() {
+        return HunterSkills;
+    }
+
+    public void setHunterSkills(boolean[] hunterSkills) {
+        HunterSkills = hunterSkills;
+    }
+
+    public boolean[] getLumberjackSkills() {
+        return LumberjackSkills;
+    }
+
+    public void setLumberjackSkills(boolean[] lumberjackSkills) {
+        LumberjackSkills = lumberjackSkills;
+    }
+
+    public boolean[] getMinerSkills() {
+        return MinerSkills;
+    }
+
+    public void setMinerSkills(boolean[] minerSkills) {
+        MinerSkills = minerSkills;
+    }
+
+    public boolean[] getMonsterHunterSkills() {
+        return MonsterHunterSkills;
+    }
+
+    public void setMonsterHunterSkills(boolean[] monsterHunterSkills) {
+        MonsterHunterSkills = monsterHunterSkills;
+    }
 }
