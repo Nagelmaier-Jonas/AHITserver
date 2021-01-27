cfb30382f52861b209bbb414a239e9cf348eb03e
Gaduso <tobias.haidvogl@gmail.com>
2021-01-27 19:16:27 +0100

    Println

--- src/at/ahit/server/main/Main.java
+++ src/at/ahit/server/main/Main.java
@@ -1,12 +1,24 @@
 package at.ahit.server.main;
 
+import at.ahit.server.main.Tobi.Listeners.RightClick;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+    private static Main plugin;
     @Override
     public void onEnable() {
+        plugin = this;
         System.out.println("Hallo");
+
+        PluginManager manager = Bukkit.getPluginManager();
+        if(!Bukkit.getOnlinePlayers().isEmpty()){
+            for (Player online : Bukkit.getOnlinePlayers())
+                manager.registerEvents(new RightClick(),this);
+        }
     }
+    public static Main getPlugin() { return plugin; }
 
 }
