4818f0b8bc5e46d33509d091d89134d366e1580e
JonasNagi <j.nagelmaier@htlkrems.at>
2021-01-28 19:35:00 +0100

    SetCoinsCommand

--- src/at/ahit/server/jobs/Miner.java
+++ src/at/ahit/server/jobs/Miner.java
@@ -122,7 +122,7 @@ public class Miner implements Listener {
         skill2Meta.setDisplayName("Skill2");
         ArrayList<String> skill2Lore = new ArrayList<String>();
         skill2Lore.add("Skill2");
-        skill2Lore.add("Costs: 5000c");
+        skill2Lore.add("Costs: 10000c");
         skill2Meta.setLore(skill2Lore);
         skill2.setItemMeta(skill2Meta);
 
@@ -131,7 +131,7 @@ public class Miner implements Listener {
         skill3Meta.setDisplayName("Skill3");
         ArrayList<String> skill3Lore = new ArrayList<String>();
         skill3Lore.add("Skill3");
-        skill3Lore.add("Costs: 10000c");
+        skill3Lore.add("Costs: 25000c");
         skill3Meta.setLore(skill3Lore);
         skill3.setItemMeta(skill3Meta);
 
