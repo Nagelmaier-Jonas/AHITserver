af78c7419a54aa534d3d5f9bba9dc9e9ebdc7b1e
Gaduso <tobias.haidvogl@gmail.com>
2021-01-28 18:57:49 +0100

    Pfad

--- src/at/ahit/server/abilities/MinerAbilities.java
+++ src/at/ahit/server/abilities/MinerAbilities.java
@@ -16,12 +16,19 @@ public class MinerAbilities implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (strings.length == 1) {
-            if (strings[0].equals("autosmelt"))
-                if ((boolean) Main.Load(commandSender.getName() + "_MinerSkill1"))
+            if (strings[0].equals("autosmelt")) {
+                if ((boolean) Main.Load(commandSender.getName() + "_MinerSkill1")) {
                     Main.Save(commandSender.getName() + "_MinerAbility1", !(boolean) Main.Load(commandSender.getName() + "_MinerAbility1"));
-            if (strings[0].equals("big"))
+                    commandSender.sendMessage("AutoSmelt toggled to " + Main.Load(commandSender.getName() + "_MinerAbility1"));
+                }
+            }
+            if (strings[0].equals("big")) {
                 if ((boolean) Main.Load(commandSender.getName() + "_MinerSkill2"))
                     Main.Save(commandSender.getName() + "_MinerAbility2", !(boolean) Main.Load(commandSender.getName() + "_MinerAbility2"));
+                {
+                    commandSender.sendMessage("Mine toggled " + Main.Load(commandSender.getName() + "_MinerAbility2"));
+                }
+            }
         }
         return false;
     }
