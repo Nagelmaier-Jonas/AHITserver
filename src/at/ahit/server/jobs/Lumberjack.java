c4838dc10ba3e034eb3c5034a94ec13f2b361c46
JonasNagi <j.nagelmaier@htlkrems.at>
2021-01-28 11:12:07 +0100

    hunter

--- src/at/ahit/server/jobs/Lumberjack.java
+++ src/at/ahit/server/jobs/Lumberjack.java
@@ -1,6 +1,15 @@
 package at.ahit.server.jobs;
 
+import at.ahit.server.main.Main;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 
 public class Lumberjack implements Listener {
+
+    @EventHandler
+    public void breakBlock(BlockBreakEvent event) {
+        int level = (int) Main.getConfigFile().get(event.getPlayer().getDisplayName() + "_LumberjackLevel");
+        int playerXp = (int)Main.getConfigFile().get(event.getPlayer().getDisplayName() + "_LumberjackXp");
+    }
 }
