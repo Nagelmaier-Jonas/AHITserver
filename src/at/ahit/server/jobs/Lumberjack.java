625294545115a18ac33c0d15dd1c00817b780bab
JonasNagi <j.nagelmaier@htlkrems.at>
2021-01-28 11:17:35 +0100

    delete simon shit

--- src/at/ahit/server/jobs/Lumberjack.java
+++ src/at/ahit/server/jobs/Lumberjack.java
@@ -11,5 +11,25 @@ public class Lumberjack implements Listener {
     public void breakBlock(BlockBreakEvent event) {
         int level = (int) Main.getConfigFile().get(event.getPlayer().getDisplayName() + "_LumberjackLevel");
         int playerXp = (int)Main.getConfigFile().get(event.getPlayer().getDisplayName() + "_LumberjackXp");
+
+        switch (event.getBlock().getType()){
+            case OAK_WOOD:
+                break;
+            case DARK_OAK_WOOD:
+                break;
+            case BIRCH_WOOD:
+                break;
+            case JUNGLE_WOOD:
+                break;
+            case SPRUCE_WOOD:
+                break;
+            case ACACIA_WOOD:
+                break;
+            case :
+                break;
+            case ACACIA_WOOD:
+                break;
+
+        }
     }
 }
