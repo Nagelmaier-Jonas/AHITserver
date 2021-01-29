f0511a02192e9d333e30ed3c68316bc433adad14
JonasNagi <j.nagelmaier@htlkrems.at>
2021-01-29 18:04:13 +0100

    some bugfixes

--- src/at/ahit/server/abilities/MinerAbilities.java
+++ src/at/ahit/server/abilities/MinerAbilities.java
@@ -8,6 +8,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+
+
 public class MinerAbilities implements CommandExecutor {
 
     @Override
