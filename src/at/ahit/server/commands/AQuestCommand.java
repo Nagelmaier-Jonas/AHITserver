4a229906eefbb73afd1f5c1a78b2842dd42b9bd9
JonasNagi <j.nagelmaier@htlkrems.at>
2021-01-29 23:26:56 +0100

    some bugfixes

--- src/at/ahit/server/commands/AQuestCommand.java
+++ src/at/ahit/server/commands/AQuestCommand.java
@@ -9,29 +9,41 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
+import org.jetbrains.annotations.NotNull;
 
 
 public class AQuestCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (commandSender instanceof Player){
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, String[] strings) {
+        if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
-            if (strings.length == 0){
+            if (strings.length == 0) {
 
-                TextComponent wizard = new TextComponent("Do you want to view your Wizard quests?");                wizard.setColor(ChatColor.GOLD);wizard.setBold(true);
-                TextComponent farmer = new TextComponent("Do you want to view your Farmer quests?");                farmer.setColor(ChatColor.GOLD);farmer.setBold(true);
-                TextComponent hunter = new TextComponent("Do you want to view your Hunter quests?");                hunter.setColor(ChatColor.GOLD);hunter.setBold(true);
-                TextComponent lumber = new TextComponent("Do you want to view your Lumber quests?");                lumber.setColor(ChatColor.GOLD);lumber.setBold(true);
-                TextComponent miner = new TextComponent("Do you want to view your Miner quests?");                  miner.setColor(ChatColor.GOLD);miner.setBold(true);
-                TextComponent monsterhunter = new TextComponent("Do you want to view your Monsterhunter quests?");  monsterhunter.setColor(ChatColor.GOLD);monsterhunter.setBold(true);
+                TextComponent wizard = new TextComponent("Do you want to view your Wizard quests?");
+                wizard.setColor(ChatColor.GOLD);
+                wizard.setBold(true);
+                TextComponent farmer = new TextComponent("Do you want to view your Farmer quests?");
+                farmer.setColor(ChatColor.GOLD);
+                farmer.setBold(true);
+                TextComponent hunter = new TextComponent("Do you want to view your Hunter quests?");
+                hunter.setColor(ChatColor.GOLD);
+                hunter.setBold(true);
+                TextComponent lumber = new TextComponent("Do you want to view your Lumber quests?");
+                lumber.setColor(ChatColor.GOLD);
+                lumber.setBold(true);
+                TextComponent miner = new TextComponent("Do you want to view your Miner quests?");
+                miner.setColor(ChatColor.GOLD);
+                miner.setBold(true);
+                TextComponent monsterhunter = new TextComponent("Do you want to view your Monsterhunter quests?");
+                monsterhunter.setColor(ChatColor.GOLD);
+                monsterhunter.setBold(true);
 
-                wizard.setClickEvent(         new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard"));
-                farmer.setClickEvent(       new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest farmer"));
-                hunter.setClickEvent(       new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest hunter"));
-                lumber.setClickEvent(       new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest lumber"));
-                miner.setClickEvent(        new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest miner"));
+                wizard.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard"));
+                farmer.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest farmer"));
+                hunter.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest hunter"));
+                lumber.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest lumber"));
+                miner.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest miner"));
                 monsterhunter.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest monsterhunter"));
 
                 wizard.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
@@ -47,34 +59,47 @@ public class AQuestCommand implements CommandExecutor {
                 monsterhunter.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
                         new ComponentBuilder("Click here to view you Monsterhunter Quests").color(ChatColor.GOLD).italic(true).create()));
 
-                player.spigot().sendMessage(wizard);player.spigot().sendMessage(farmer);
-                player.spigot().sendMessage(hunter);player.spigot().sendMessage(lumber);
-                player.spigot().sendMessage(miner);player.spigot().sendMessage(monsterhunter);
+                player.spigot().sendMessage(wizard);
+                player.spigot().sendMessage(farmer);
+                player.spigot().sendMessage(hunter);
+                player.spigot().sendMessage(lumber);
+                player.spigot().sendMessage(miner);
+                player.spigot().sendMessage(monsterhunter);
 
                 return true;
             }
 
-            if (strings[0].equals("wizard") && strings[1].equals("")){
-                TextComponent wizardq1 = new TextComponent("Do a magic trick");wizardq1.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q1"));
-                TextComponent wizardq2 = new TextComponent("Shit someone in their house");wizardq2.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q2"));
-                TextComponent wizardq3 = new TextComponent("Summon the Weathergod");wizardq3.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q3"));
-                TextComponent wizardq4 = new TextComponent("Get kicked for Flying");wizardq4.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q4"));
-                TextComponent wizardq5 = new TextComponent("Get hit by lightning");wizardq5.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q5"));
+            if (strings[0].equals("wizard") && strings[1].equals("")) {
+                TextComponent wizardq1 = new TextComponent("Do a magic trick");
+                wizardq1.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q1"));
+                TextComponent wizardq2 = new TextComponent("Shit someone in their house");
+                wizardq2.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q2"));
+                TextComponent wizardq3 = new TextComponent("Summon the Weathergod");
+                wizardq3.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q3"));
+                TextComponent wizardq4 = new TextComponent("Get kicked for Flying");
+                wizardq4.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q4"));
+                TextComponent wizardq5 = new TextComponent("Get hit by lightning");
+                wizardq5.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/aquest wizard q5"));
                 wizardq1.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));player.spigot().sendMessage(wizardq1);
+                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));
+                player.spigot().sendMessage(wizardq1);
                 wizardq2.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));player.spigot().sendMessage(wizardq2);
+                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));
+                player.spigot().sendMessage(wizardq2);
                 wizardq3.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));player.spigot().sendMessage(wizardq3);
+                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));
+                player.spigot().sendMessage(wizardq3);
                 wizardq4.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));player.spigot().sendMessage(wizardq4);
+                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));
+                player.spigot().sendMessage(wizardq4);
                 wizardq5.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));player.spigot().sendMessage(wizardq5);
+                        new ComponentBuilder("Click here to complete the Quest").color(ChatColor.GOLD).italic(true).create()));
+                player.spigot().sendMessage(wizardq5);
 
 
             }
-            if (strings[0].equals("wizard")){
-                switch (strings[1]){
+            if (strings[0].equals("wizard")) {
+                switch (strings[1]) {
                     case "q1":
                         break;
                     case "q2":
@@ -89,8 +114,8 @@ public class AQuestCommand implements CommandExecutor {
                 }
             }
 
-            if (strings[0].equals("farmer")){
-                switch (strings[1]){
+            if (strings[0].equals("farmer")) {
+                switch (strings[1]) {
                     case "q1":
                         break;
                     case "q2":
@@ -106,30 +131,29 @@ public class AQuestCommand implements CommandExecutor {
             }
 
 
-            if (strings[0].equals("farmer")){
+            if (strings[0].equals("farmer")) {
                 player.sendMessage(ChatColor.GREEN + "Collect you first seeds");
             }
 
-            if (strings[0].equals("hunter")){
+            if (strings[0].equals("hunter")) {
                 player.sendMessage(ChatColor.GREEN + "Kill you first Animals");
             }
 
-            if (strings[0].equals("lumber")){
+            if (strings[0].equals("lumber")) {
                 player.sendMessage(ChatColor.GREEN + "Get some Wood");
             }
 
-            if (strings[0].equals("miner")){
+            if (strings[0].equals("miner")) {
                 player.sendMessage(ChatColor.GREEN + "Mine some cobblestone");
             }
 
-            if (strings[0].equals("monsterhunter")){
+            if (strings[0].equals("monsterhunter")) {
                 player.sendMessage(ChatColor.GREEN + "Kill your first Monsters");
             }
             return true;
 
 
-        }
-        else {
+        } else {
             commandSender.sendMessage("Console mach was anderes sonst Faust");
         }
         return false;
--- src/at/ahit/server/overlays/Scoreboards.java
+++ src/at/ahit/server/overlays/Scoreboards.java
@@ -1,5 +1,6 @@
 package at.ahit.server.overlays;
 
+import at.ahit.server.main.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -9,6 +10,7 @@ import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Scoreboards {
@@ -17,22 +19,19 @@ public class Scoreboards {
         Scoreboard scoreboard = Objects.requireNonNull(Bukkit.getScoreboardManager()).getNewScoreboard();
         Objective objective = scoreboard.registerNewObjective("Scoreboard","dummy","§6§l>>Info<<");
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
-        Score score1 = objective.getScore(ChatColor.DARK_RED + "-=-=-=-=-=-=-=-");
-        score1.setScore(6);
-        Score score2 = objective.getScore(ChatColor.BLUE + "");
-        score2.setScore(5);
-        Score score3 = objective.getScore(ChatColor.BLUE + "Player Name: " + ChatColor.AQUA + player.getDisplayName());
-        score3.setScore(4);
-        Score score4 = objective.getScore(ChatColor.BLUE + "Online Players: " + ChatColor.AQUA + Bukkit.getOnlinePlayers().size());
-        score4.setScore(3);
-        Score score5 = objective.getScore(ChatColor.BLUE + "Coins: " + ChatColor.AQUA + config.get(player.getDisplayName() + "_Amount"));
+        Score score1 = objective.getScore(ChatColor.BLUE + "");
+        score1.setScore(5);
+        Score score2 = objective.getScore(ChatColor.BLUE + "Player Name: " + ChatColor.AQUA + player.getDisplayName());
+        score2.setScore(4);
+        Score score3 = objective.getScore(ChatColor.BLUE + "Online Players: " + ChatColor.AQUA + Bukkit.getOnlinePlayers().size());
+        score3.setScore(3);
+        Score score4 = objective.getScore(ChatColor.BLUE + "Coins: " + ChatColor.AQUA + config.get(player.getDisplayName() + "_Amount"));
+        score4.setScore(2);
+        Score score5 = objective.getScore(ChatColor.BLUE + "Latest Job: " + Main.Load(player.getDisplayName() +"_LatestJob") + ":" + Main.Load(player.getDisplayName() + "_" + Main.Load(player.getDisplayName()) + "_LatestJob") + "Level");
         score5.setScore(2);
         Score score6 = objective.getScore(ChatColor.RED + "");
         score6.setScore(1);
-        Score score7 = objective.getScore(ChatColor.DARK_RED + "-=-=-=-=-=-=-=- ");
-        score7.setScore(0);
         player.setScoreboard(scoreboard);
     }
-
 
 }
