ac1c0e1e7ae061ef5d23d686b448e224f773dece
Gaduso <tobias.haidvogl@gmail.com>
2021-01-30 23:54:32 +0100

    ?????????????

--- src/at/ahit/server/overlays/Auction.java
+++ src/at/ahit/server/overlays/Auction.java
@@ -1,7 +1,10 @@
 package at.ahit.server.overlays;
 
+import at.ahit.server.jobs.Lumberjack;
+import at.ahit.server.main.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
@@ -9,12 +12,15 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Auction {
 
     public static void openAuctionMenu(Player player) {
-        Inventory AuctionInventory = Bukkit.createInventory(null, 54, "§4Auction");
+        /*Inventory AuctionInventory = Bukkit.createInventory(null, 54, "§4Auction");
 
         ItemStack freeSlot = new ItemStack(Material.LIME_STAINED_GLASS_PANE,1);
         ItemMeta freeSlotMeta = freeSlot.getItemMeta();
@@ -42,7 +48,22 @@ public class Auction {
         AuctionInventory.setItem(45,previous);
         AuctionInventory.setItem(53,next);
 
-        player.openInventory(AuctionInventory);
+        player.openInventory(AuctionInventory);*/
+        ArrayList<ItemStack> items = new ArrayList<>();
+
+        items.add(SkillMenu.createItem(Material.ARROW, 1, "Previous"));
+        items.add(SkillMenu.createItem(Material.ARROW, 1, "Next"));
+        items.add(SkillMenu.createItem(Material.LIME_STAINED_GLASS_PANE, 1, "FreeSlot"));
+
+        Inventory inventory = SkillMenu.createSkillInventory(player, "§4Auction", new HashMap<Integer, ItemStack>() {{
+            for (int i = 0; i < 45; ++i){
+                put(i,items.get(2));
+            }
+            put(45, items.get(0));
+            put(53, items.get(1));
+        }}, 54);
+        player.openInventory(inventory);
+
     }
 
     public static void onAuctionUse(InventoryClickEvent event){
